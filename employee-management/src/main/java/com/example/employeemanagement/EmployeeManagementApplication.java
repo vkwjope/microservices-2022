af42fc5f38f5c8bdcbaefff576203ad372d3234c
Vinod Kumar <58595080+vkwjope@users.noreply.github.com>
2022-11-24 19:31:33 +0530

    Changes for API Gateway and Service discovery

--- employee-management/src/main/java/com/example/employeemanagement/EmployeeManagementApplication.java
+++ employee-management/src/main/java/com/example/employeemanagement/EmployeeManagementApplication.java
@@ -2,9 +2,10 @@ package com.example.employeemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableSwagger2
+@EnableEurekaClient
 public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
